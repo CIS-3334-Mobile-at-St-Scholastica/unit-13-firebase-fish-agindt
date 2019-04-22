6fdfc6161dbcb63dee5e1ecf424f7015c1b32509
agindt <agindt@css.edu>
2019-04-22 14:46:01 -0500

    Completed participation 13 (WORKING!)

--- app/src/main/java/edu/css/unit13_firebasefish_2018/FishFirebaseData.java
+++ app/src/main/java/edu/css/unit13_firebasefish_2018/FishFirebaseData.java
@@ -1,5 +1,9 @@
 package edu.css.unit13_firebasefish_2018;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,17 +12,20 @@ import java.util.List;
  */
 
 public class FishFirebaseData {
-//    DatabaseReference myFishDbRef;
+    DatabaseReference myFishDbRef;
     public static final String FishDataTag = "Fish Data";
 
-    /*
+
     public DatabaseReference open()  {
         // Write a message to the database
+        //Get instance of firebase database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //Get a reference to fishdata using the tag
         myFishDbRef = database.getReference(FishDataTag);
+        //return the reference
         return myFishDbRef;
     }
-    */
+
 
     public void close() {
 
@@ -26,41 +33,44 @@ public class FishFirebaseData {
 
     public Fish createFish( String species, String weightInOz, String dateCaught) {           //Added String rating as a parameter
         // ---- Get a new database key for the vote
-//        String key = myFishDbRef.child(FishDataTag).push().getKey();
-        String key = "REPLACE THIS WITH KEY FROM DATABASE";
+        String key = myFishDbRef.child(FishDataTag).push().getKey();
+        //String key = "REPLACE THIS WITH KEY FROM DATABASE";
         // ---- set up the fish object
         Fish newFish = new Fish(key, species, weightInOz, dateCaught);
         // ---- write the vote to Firebase
-//        myFishDbRef.child(key).setValue(newFish);
+        myFishDbRef.child(key).setValue(newFish);
         return newFish;
     }
 
     public Fish createFish( String species, String weightInOz, String dateCaught, String locationLatitude, String locationLongitude) {           //Added String rating as a parameter
         // ---- Get a new database key for the vote
-//        String key = myFishDbRef.child(FishDataTag).push().getKey();
-        String key = "REPLACE THIS WITH KEY FROM DATABASE";
+        String key = myFishDbRef.child(FishDataTag).push().getKey();
+//        String key = "REPLACE THIS WITH KEY FROM DATABASE";
         // ---- set up the fish object
         Fish newFish = new Fish(key, species, weightInOz, dateCaught, locationLatitude,locationLongitude);
         // ---- write the vote to Firebase
-//        myFishDbRef.child(key).setValue(newFish);
+        myFishDbRef.child(key).setValue(newFish);
         return newFish;
     }
 
     public void deleteFish(Fish fish) {
         String key = fish.getKey();
-//        myFishDbRef.child(key).removeValue();
+        //Remove the value in database associated with the key
+        myFishDbRef.child(key).removeValue();
     }
 
-    /*
+
     public List<Fish> getAllFish(DataSnapshot dataSnapshot) {
+        //Create a list of fish objects
         List<Fish> fishList = new ArrayList<Fish>();
+        //loop through data returned in the datasnapshot
         for (DataSnapshot data : dataSnapshot.getChildren()) {
             Fish fish = data.getValue(Fish.class);
             fishList.add(fish);
         }
         return fishList;
     }
-    */
+
 
 }
 
